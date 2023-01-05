840eddac66319094d433222015201884b6153b4e
Adem CANSIZ <ademmcansiz@gmail.com>
2023-01-06 00:35:15 +0300

    updates

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Counter } from './Counter';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Counter></Counter>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+function Footer() {
+
+    const data = useSelector((state) => state.counter.value)
+  return (
+    <div>
+      Globalden gelen veri :  {data}
+    </div>
+  )
+}
+
+export default Footer
